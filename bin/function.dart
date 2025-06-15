6be08844d344f7b511169ff2d00dfec8c29501bc
programmernusantara <pronus.programmernusantara@gmail.com>
2025-06-16 05:34:22 +0700

    function

---
+++ bin/function.dart
@@ -0,0 +1,15 @@
+//Deklarasi Function
+void hello() {
+  print('Hello Word!');
+}
+
+//Parameter Function
+void hai(String name) {
+  print('Hai, $name');
+}
+
+//
+void main() {
+  hello();
+  hai('Wildan');
+}
