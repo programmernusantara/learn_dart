38c9ac9ef16291f7ccbe4fc2841cbfdc8943861b
Wildan FQ <168396824+programmernusantara@users.noreply.github.com>
2025-07-06 02:16:55 +0700

    First Commit

---
+++ bin/condition/relational.dart
@@ -0,0 +1,12 @@
+void main() {
+  int a = 10;
+  int b = 5;
+
+  if (a <= b) {
+    print('$a: greater than $b');
+  } else if (a >= b) {
+    print('$a: Smaller than $b');
+  } else {
+    print('Error');
+  }
+}
