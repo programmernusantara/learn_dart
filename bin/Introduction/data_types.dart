0fad2375abbdd10de1be70b7bc83641062f4f8d4
programmernusantara <programmernusantara@gmail.com>
2025-07-05 20:55:19 +0700

    First Commit

---
+++ bin/Introduction/data_types.dart
@@ -0,0 +1,11 @@
+void main() {
+  int num = 10;
+  double precision = 100.00;
+  String alfabet = 'Wildan';
+  List list = [1, 2, 3];
+  Map map = {'First': 'Wildan', 'Midle': 'Firmani', 'Last': 'Quraisi'};
+
+  print(
+    'number: $num, Desimal: $precision, Alfabet: $alfabet, List: $list, Map: $map',
+  );
+}
---
+++ bin/Introduction/variable.dart
@@ -0,0 +1,4 @@
+void main() {
+  var name = 'Wildan';
+  print('Hi, my name is $name');
+}
