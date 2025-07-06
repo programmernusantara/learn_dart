b5906bd365b5077305d6f6b481be2a2d8f1bd54f
Wildan FQ <168396824+programmernusantara@users.noreply.github.com>
2025-07-07 01:54:08 +0700

    First Commit

---
+++ bin/control_flow/switch_statements.dart
@@ -0,0 +1,12 @@
+void main() {
+  var dayofweek = 1;
+  switch (dayofweek) {
+    case 1:
+      print('Day Is Sunday');
+      break;
+    case 2:
+      print('Day Is Monday');
+      break;
+    default:
+  }
+}
